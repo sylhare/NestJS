2c23cf3a67ba052f48ef2bd04bb7a877556eedb2
sylhare <sylhare@outlook.com>
2024-04-22 17:03:17 -0400

    Add IsNotRobert validator

--- src/book/entities/author.entity.ts
+++ src/book/entities/author.entity.ts
@@ -1,15 +1,40 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { Book } from './book.entity';
+import {
+  registerDecorator,
+  ValidationOptions,
+} from 'class-validator';
 
 @ObjectType({ description: 'Author' })
 export class Author {
+  @IsNotRobert()
   @Field({ description: 'name of the author' })
   name: string;
   @Field(() => [Book], { description: 'Author of the Book' })
   books: Book[];
 
-  constructor() {
+  constructor(name?: string) {
     this.books = [new Book(1), new Book(2)];
-    this.name = 'author';
+    this.name = name ?? 'author';
   }
+}
+
+export function IsNotRobert(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'IsNotRobert',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [],
+      options: {
+        message: 'name must not be Robert',
+        ...validationOptions,
+      },
+      validator: {
+        validate(value: any) {
+          return value !== 'Robert';
+        },
+      },
+    });
+  };
 }
