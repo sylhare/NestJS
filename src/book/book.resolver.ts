4fc434e9474f97704499c49b4c644ff641be1d85
sylhare <sylhare@outlook.com>
2024-04-24 16:50:02 -0400

    Add CreateAuthorMutation

--- src/book/book.resolver.ts
+++ src/book/book.resolver.ts
@@ -1,7 +1,8 @@
-import { Args, Context, Int, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { Args, Context, Int, Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
 import { Book } from './entities/book.entity';
 import { Logger } from '@nestjs/common';
 import { Author } from './entities/author.entity';
+import { CreateAuthorInput } from './dto/create-author.dto';
 
 @Resolver(() => Book)
 export class BookResolver {
@@ -26,5 +27,10 @@ export class AuthorResolver {
   @ResolveField('books', () => [Book])
   async getBooks(@Parent() author: Author): Promise<Book[]> {
     return author.books;
+  }
+
+  @Mutation(() => Author)
+  async createAuthor(@Args('input') input: CreateAuthorInput): Promise<Author> {
+    return new Author(input.name);
   }
 }
