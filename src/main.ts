d158a232020191751fd456b4323cfd8eccd76713
sylhare <sylhare@outlook.com>
2024-04-25 09:48:13 -0400

    Add ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+export const validationPipe = new ValidationPipe({
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(validationPipe);
+  await app.listen(3001);
 }
 
 bootstrap();
