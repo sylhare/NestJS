d7adc532510e0079478dcfbff59481e043b82f1d
sylhare <sylhare@outlook.com>
2024-05-30 09:59:25 -0400

    Add job entities and relation

---
+++ src/typeorm/entities/job.entity.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { UserToJob } from './user-to-job.entity';
+
+@Entity('nest_jobs')
+export class Job {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @OneToMany(() => UserToJob, (userToJob) => userToJob.job)
+  jobToUsers: UserToJob[];
+
+}
---
+++ src/typeorm/entities/user-to-job.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, JoinColumn } from 'typeorm';
+import { User } from './user.entity';
+import { Job } from './job.entity';
+
+@Entity('users_to_jobs')
+export class UserToJob {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  userId: number;
+
+  @Column()
+  jobId: number;
+
+  @ManyToOne(() => User, (user) => user.userToJobs)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @ManyToOne(() => Job, (job) => job.jobToUsers)
+  @JoinColumn({ name: 'jobId' })
+  job: Job;
+}
