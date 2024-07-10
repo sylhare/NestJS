5c161aa35598bf7c959d9bd62250bd7e73d98d63
sylhare <sylhare@outlook.com>
2024-07-10 17:08:02 -0400

    Update database service

--- src/typeorm/database.service.ts
+++ src/typeorm/database.service.ts
@@ -3,12 +3,14 @@ import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { plainToInstance } from 'class-transformer';
 import { Logger } from '@nestjs/common';
+import { Job } from './entities/job.entity';
 
 export class DatabaseService {
   private readonly logger = new Logger(DatabaseService.name);
 
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @InjectRepository(Job) private readonly jobRepository: Repository<Job>,
   ) {}
 
   async createUser(name: string): Promise<User> {
@@ -19,5 +21,20 @@ export class DatabaseService {
 
   async getUsers(): Promise<User[]> {
     return this.userRepository.find();
+  }
+
+  async getUserById(id: string): Promise<User> {
+    return this.userRepository.findOne({
+      where: { id },
+      relations: ['home', 'userToJobs', 'userToJobs.job'],
+    });
+  }
+
+  async getJobsById(id: number): Promise<Job> {
+    //return this.userRepository.manager.findOne(Job, {
+    return this.jobRepository.findOne({
+      where: { id },
+      relations: ['roles', 'jobToUsers', 'jobToUsers.user'],
+    });
   }
 }
